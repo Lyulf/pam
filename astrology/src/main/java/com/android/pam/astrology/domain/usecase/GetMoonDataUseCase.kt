38ef62afa39388f2d5bf0d6a7975660fa762da03
Lyulf <patryk.chodorowski0@gmail.com>
2020-04-16 16:17:08 +0200

    Add GetMoonDataUseCase

--- astrology/src/main/java/com/android/pam/astrology/domain/usecase/GetMoonDataUseCase.kt
+++ astrology/src/main/java/com/android/pam/astrology/domain/usecase/GetMoonDataUseCase.kt
@@ -1,3 +1,13 @@
 package com.android.pam.astrology.domain.usecase
 
-class GetMoonDataUseCase
+import com.android.pam.astrology.domain.model.moon.Moon
+import com.android.pam.astrology.domain.wrapper.IAstrology
+import javax.inject.Inject
+
+class GetMoonDataUseCase @Inject constructor(
+    private val data: IAstrology
+) {
+    fun invoke(): Moon {
+        return data.moon()
+    }
+}
