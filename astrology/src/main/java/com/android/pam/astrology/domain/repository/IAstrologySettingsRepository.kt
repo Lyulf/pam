e7fbf2d009d4b2bead5a886adca503652e06fcb4
Lyulf <patryk.chodorowski0@gmail.com>
2020-04-16 16:22:26 +0200

    Add SaveAstrologySettingsUseCase

--- astrology/src/main/java/com/android/pam/astrology/domain/repository/IAstrologySettingsRepository.kt
+++ astrology/src/main/java/com/android/pam/astrology/domain/repository/IAstrologySettingsRepository.kt
@@ -4,6 +4,7 @@ import com.android.pam.astrology.domain.model.settings.Location
 import com.android.pam.astrology.domain.model.settings.Settings
 
 interface IAstrologySettingsRepository {
+    fun setSettings(settings: Settings)
     fun getSettings(): Settings
     fun getLocation(): Location
     fun getRefreshRate(): Int
--- astrology/src/main/java/com/android/pam/astrology/domain/usecase/SaveAstrologySettingsUseCase.kt
+++ astrology/src/main/java/com/android/pam/astrology/domain/usecase/SaveAstrologySettingsUseCase.kt
@@ -1,5 +1,13 @@
 package com.android.pam.astrology.domain.usecase
 
-class SaveAstrologySettingsUseCase {
+import com.android.pam.astrology.domain.model.settings.Settings
+import com.android.pam.astrology.domain.repository.IAstrologySettingsRepository
+import javax.inject.Inject
 
+class SaveAstrologySettingsUseCase @Inject constructor(
+    private val repository: IAstrologySettingsRepository
+) {
+    fun invoke(settings: Settings) {
+        repository.setSettings(settings)
+    }
 }
