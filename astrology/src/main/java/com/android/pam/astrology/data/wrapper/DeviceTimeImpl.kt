eb3654b95a44a1096a028cf017b28c1b06baa9dd
Lyulf <patryk.chodorowski0@gmail.com>
2020-04-16 17:16:39 +0200

    Add DeviceTimeImpl

--- astrology/src/main/java/com/android/pam/astrology/data/wrapper/DeviceTimeImpl.kt
+++ astrology/src/main/java/com/android/pam/astrology/data/wrapper/DeviceTimeImpl.kt
@@ -1,7 +1,12 @@
 package com.android.pam.astrology.data.wrapper
 
 import com.android.pam.astrology.domain.wrapper.IDeviceTime
+import java.sql.Time
+import java.util.*
 
 class DeviceTimeImpl : IDeviceTime {
+    override fun currentTime(): Time {
+        return Time(Calendar.getInstance().timeInMillis)
+    }
 
 }
