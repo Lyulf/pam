bf1fb1234e9fde55badad78bde71ac8a770c27fc
Lyulf <patryk.chodorowski0@gmail.com>
2020-04-16 16:15:16 +0200

    Add GetSunDataUseCase

--- astrology/src/main/java/com/android/pam/astrology/domain/usecase/GetSunDataUseCase.kt
+++ astrology/src/main/java/com/android/pam/astrology/domain/usecase/GetSunDataUseCase.kt
@@ -1,3 +1,13 @@
 package com.android.pam.astrology.domain.usecase
 
-class GetSunDataUseCase
+import com.android.pam.astrology.domain.model.sun.Sun
+import com.android.pam.astrology.domain.wrapper.IAstrology
+import javax.inject.Inject
+
+class GetSunDataUseCase @Inject constructor(
+    private val data: IAstrology
+) {
+    fun invoke(): Sun {
+        return data.sun()
+    }
+}
