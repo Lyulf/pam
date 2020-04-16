0d47a1f4a83bd04d51f8b36520e9e4c1ce7e99e7
Lyulf <patryk.chodorowski0@gmail.com>
2020-04-16 18:43:03 +0200

    Add updating wrapper settings to UseCase

--- astrology/src/main/java/com/android/pam/astrology/domain/usecase/SaveAstrologySettingsUseCase.kt
+++ astrology/src/main/java/com/android/pam/astrology/domain/usecase/SaveAstrologySettingsUseCase.kt
@@ -2,12 +2,15 @@ package com.android.pam.astrology.domain.usecase
 
 import com.android.pam.astrology.domain.model.settings.Settings
 import com.android.pam.astrology.domain.repository.IAstrologySettingsRepository
+import com.android.pam.astrology.domain.wrapper.IAstrology
 import javax.inject.Inject
 
 class SaveAstrologySettingsUseCase @Inject constructor(
-    private val repository: IAstrologySettingsRepository
+    private val repository: IAstrologySettingsRepository,
+    private val wrapper: IAstrology
 ) {
     fun invoke(settings: Settings) {
         repository.setSettings(settings)
+        wrapper.updateSettings()
     }
 }
